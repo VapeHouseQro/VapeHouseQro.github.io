8d96b968a8c1a919449ced372d8b43afa8f3ed7a
VapeHouseQro <miguel.castillo1278@gmail.com>
2024-12-01 23:12:04 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// Selección de los botones de productos para abrir los modales
 const modalWaka = document.getElementById('modal-waka');
 const modalIplay = document.getElementById('modal-iplay');
 const modalAirfuze = document.getElementById('modal-airfuze');
 const modalNastyBar = document.getElementById('modal-nastybar');
 
-// Selección de las imágenes de los productos
 const wakaBtn = document.querySelector('.producto:nth-child(1)');
 const iplayBtn = document.querySelector('.producto:nth-child(2)');
 const airfuzeBtn = document.querySelector('.producto:nth-child(3)');
 const nastybarBtn = document.querySelector('.producto:nth-child(4)');
 
-// Función para abrir el modal con transición suave
 function openModal(model) {
   const modal = document.getElementById('modal-' + model);
-  modal.classList.add('mostrar'); // Añadir clase 'mostrar' para hacer visible el modal
+  modal.classList.add('mostrar');
 }
 
-// Función para cerrar el modal con transición suave
 function closeModal(model) {
   const modal = document.getElementById('modal-' + model);
-  modal.classList.remove('mostrar'); // Eliminar clase 'mostrar' para ocultar el modal
+  modal.classList.remove('mostrar'); 
 }
 
-// Funciones para abrir los modales
 wakaBtn.addEventListener('click', () => {
   openModal('waka');
 });
@@ -39,22 +34,20 @@ nastybarBtn.addEventListener('click', () => {
   openModal('nastybar');
 });
 
-// Función para cerrar los modales desde la X
 const cerrarModales = document.querySelectorAll('.cerrar');
 
 cerrarModales.forEach(cerrar => {
   cerrar.addEventListener('click', () => {
-    const modalName = cerrar.closest('.modal').id.split('-')[1]; // Obtener el nombre del modal
+    const modalName = cerrar.closest('.modal').id.split('-')[1];
     closeModal(modalName);
   });
 });
 
-// Cerrar el modal al hacer clic fuera del área de contenido
 window.addEventListener('click', (event) => {
   const modals = document.querySelectorAll('.modal');
   modals.forEach(modal => {
     if (event.target === modal) {
-      modal.classList.remove('mostrar'); // Cerrar el modal si se hace clic fuera
+      modal.classList.remove('mostrar');
     }
   });
 });
